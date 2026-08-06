3fed1eaf02b5a36c7389b26c04430e72f25de327
Sara Vitoria Ribeiro Da Silva <saravitoriaribeiro1819@gmail.com>
2026-08-06 11:40:27 -0300

    primeirocommit

---
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,35 @@
+import { Component, signal, computed } from '@angular/core';
+import { Produto } from '../produto/produto';
+
+@Component({
+  selector: 'app-lista-produtos',
+  imports: [Produto],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css',
+})
+export class ListaProdutos {
+  produtos = signal([
+    { nome: 'Notebook', preco: 3800 },
+    { nome: 'Mouse', preco: 179 },
+    { nome: 'Fone de ouvido', preco: 80 },
+  ]);
+  //
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
+
+  exibirProduto(nome: string) {
+    console.log('Produto selecionado:', nome);
+    // Aqui você pode atualizar o estado, abrir modal, etc.
+  }
+
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
+  }
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
+  }
+
+}
