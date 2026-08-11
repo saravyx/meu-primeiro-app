6a04061ea7fbb352fcd024a806ecbe295147db5e
Sara Vitoria Ribeiro Da Silva <saravitoriaribeiro1819@gmail.com>
2026-08-11 09:20:44 -0300

    rotas do e-commerce - componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
-
-  import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+  import { Home } from './features/home/home/home';
   import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
   import { Carrinho } from './features/carrinho/carrinho/carrinho';
 
   export const routes: Routes = [
-    {
-      path: '',
-      component: ListaProdutos,
-    },
-    {
-      path: 'carrinho',
-      component: Carrinho,
-    },
+    { path: '', component: Home },
+    { path: 'produtos', component: ListaProdutos },
+    { path: 'carrinho', component: Carrinho },
   ];
