b8b023885f0ee16371557dddebc9d9cb693f56b7
Sara Vitoria Ribeiro Da Silva <saravitoriaribeiro1819@gmail.com>
2026-08-18 09:07:10 -0300

    formulário de checkout

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,37 +1,71 @@
-import { Component } from '@angular/core';
 
-  import {
-    ReactiveFormsModule,
-    FormGroup,
-    FormControl,
-    Validators,
-    AbstractControl,
-    ValidationErrors,
-  } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+
+import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
-export class Checkout {}
- function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
+export class Checkout {
+  carrinhoService = inject(CarrinhoService);
 
-    const valor = control.value;
+  compraFinalizada = signal(false);
 
-    if (!valor) return null;
-
-    if (/\d/.test(valor)) {
-      return { numeroInvalido: true };
-    }
-
-    return null;
-  }
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
     email: new FormControl('', [Validators.required, Validators.email]),
     endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
- 
+
+  finalizar() {
+    this.compraFinalizada.set(false);
+
+    if (this.carrinhoService.carrinhoVazio()) {
+      console.log('Não é possível finalizar uma compra com o carrinho vazio.');
+      return;
+    }
+
+    if (this.formulario.invalid) {
+      console.log('Formulário inválido');
+      this.formulario.markAllAsTouched();
+      return;
+    }
+
+    const dados = this.formulario.value;
+    const itens = this.carrinhoService.itens();
+    const total = this.carrinhoService.total();
+
+    console.log('Compra finalizada com sucesso!');
+    console.log('Dados do formulário:', dados);
+    console.log('Itens do carrinho:', itens);
+    console.log('Total da compra:', total);
+
+    this.carrinhoService.limpar();
+    this.formulario.reset();
+    this.compraFinalizada.set(true);
+  }
+}
+
+function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
+  const valor = control.value;
+
+  if (!valor) return null;
+
+  if (/\d/.test(valor)) {
+    return { numeroInvalido: true };
+  }
+
+  return null;
+}
 
